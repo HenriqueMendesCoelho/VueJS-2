f9437d64e32e011b3599a812b53a1ed4006737e2
unknown <HenriqueMendes@merak.icarotech.com>
2022-03-30 18:10:52 -0300

    Primeiro commit

---
+++ src/components/shared/painel/Painel.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="painel">
+        <!-- v-on pode ser escrito tmb apenas com um @ -->
+        <h2 class="painel-titulo" @click="visible = !visible">{{ titulo }}</h2>
+        <transition name="painel-fade">
+            <div class="painel-conteudo" v-show="visible">
+                <slot></slot>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['titulo'],
+
+        data() {
+            return {
+                visible : true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .painel{
+        border: solid 1px whitesmoke;
+        padding: 0 auto;
+        margin: 5px;
+        vertical-align: top;
+        width: 200px;
+        height: 100%;
+        display: inline-block;
+        background-color: rgb(83, 22, 99);
+    }
+    .painel-titulo{
+        text-align: center;
+        text-align-last: center;
+        vertical-align: top;
+        height: 45px;
+        font-size: 20px;
+        border-block-end: solid 1px whitesmoke;
+    }
+</style>
